d4a61cb8747450026a2ff9f155c6cbdf1b4eb9ac
i-panov <panov210@yandex.ru>
2017-09-20 12:00:00 +0300

    Изменил название проекта и README

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "motmom_cafe.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafe_hub.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
